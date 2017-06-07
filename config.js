31b39fbf908c2a9f29b91f5fcd21cc9cfe146e5e
plearnio <p.plearn.io@gmail.com>
2017-06-07 11:38:01 +0700

    change config

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mongo: {
+    host: 'localhost',
+    port: '27017',
+    database: 'internjitta',
+    collection: 'books',
+    nameCollection: 'Book'
+  }
+}
--- models/config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  host: 'localhost',
-  port: '27017',
-  database: 'internjitta',
-  collections: ['books']
-}
